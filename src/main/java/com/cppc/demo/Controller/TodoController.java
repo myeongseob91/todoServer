b697daad8c555c6f175b2f6e4d33b20756c7bdf1
moongee <lyoung455@hanmail.net>
2019-03-06 21:05:16 +0900

    ..

--- src/main/java/com/cppc/demo/Controller/TodoController.java
+++ src/main/java/com/cppc/demo/Controller/TodoController.java
@@ -4,6 +4,8 @@ import com.cppc.demo.Service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -14,12 +16,13 @@ public class TodoController {
     @Autowired
     TodoService todoService;
 
-    @GetMapping("/todo")
+    @GetMapping("/todo/{todoId}")
     @ResponseBody
-    public List<Map<String, Object>> getEmployee() {
-        System.out.println(todoService.getTodo());
+    public List<Map<String, Object>> getEmployee(@PathVariable("todoId") String todoId) {
+        System.out.println("todoId:::::::::::::::::::::::::"+todoId);
         return todoService.getTodo();
     }
 
 
 }
+//
